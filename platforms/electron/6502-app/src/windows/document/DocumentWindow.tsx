6763bd61f95f086a17c3c96c19eb47715caffa6a
Rakesh TA <rakeshta@gmail.com>
2020-05-23 03:11:46 +1000

    eectron-app: reading file data

--- platforms/electron/6502-app/src/windows/document/DocumentWindow.tsx
+++ platforms/electron/6502-app/src/windows/document/DocumentWindow.tsx
@@ -6,16 +6,63 @@
 //  Copyright (c) 2020 Raptor Soft. All rights reserved.
 //
 
-import React         from 'react';
+import React          from 'react';
 
-import IpcListener   from '../../components/utility/IpcListener';
+import fs             from 'fs';
+import path           from 'path';
+import { promisify }  from 'util';
 
+import IpcListener    from '../../components/utility/IpcListener';
+
+
+const readFile = promisify(fs.readFile);
+
+
+// class DocumentWindow ------------------------------------------------------------------------------------------------
 
 export interface Props {
-    filePath:    string;
+    filePath?:    string;
+    newFileName?: string;
 }
 
-export default class DocumentWindow extends React.PureComponent<Props> {
+interface State {
+    filePath?:    string;
+    fileName?:    string;
+}
+
+export default class DocumentWindow extends React.PureComponent<Props, State> {
+
+    public state: State = {};
+
+
+    // lifecycle -------------------------------------------------------------------------------------------------------
+
+    public componentDidMount(): void {
+        super.componentDidMount?.();
+        this._reload();
+    }
+
+
+    // load ------------------------------------------------------------------------------------------------------------
+
+    private async _reload(): Promise<void> {
+
+        // load file if given
+        const filePath     = this.state.filePath || this.props.filePath;
+        if (filePath) {
+            const contents = await readFile(filePath, 'utf8');
+            console.group('--debug file', filePath, '\n', contents);
+            const fileName = path.basename(filePath);
+            this.setState({fileName, filePath});
+        }
+
+        // set filename if new file
+        else if (this.props.newFileName) {
+            const fileName = this.props.newFileName;
+            this.setState({fileName});
+        }
+    }
+
 
     // events ----------------------------------------------------------------------------------------------------------
 
@@ -27,10 +74,11 @@ export default class DocumentWindow extends React.PureComponent<Props> {
     // render ----------------------------------------------------------------------------------------------------------
 
     public render(): React.ReactNode {
+        const {fileName} = this.state;
         return (
             <div className='window welcome'>
                 <IpcListener channel='menu.save' listener={this._ipc_onSave}/>
-                <h1>Document Window: {this.props.filePath}</h1>
+                <h1>Document Window: {fileName}</h1>
             </div>
         );
     }
